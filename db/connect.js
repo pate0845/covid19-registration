685ee81bc82c3db9727656e3c5daecf212770c78
pate0845 <pate0845@algonquinlive.com>
2022-03-20 19:10:24 -0400

    .buffer time out

--- db/connect.js
+++ db/connect.js
@@ -2,7 +2,9 @@ const mongoose = require("mongoose")
 require('dotenv').config();
 
 const connectDB=()=>{
-    return mongoose.connect(process.env.MONGO_URI)
+    return mongoose.connect(process.env.MONGO_URI,{useNewUrlParser:true,
+        useUnifiedTopology:true,
+        useCreateIndex:true})
     .then(()=>{console.log('Connected to database')})
     .catch((e)=>{console.log(e)});
 }
