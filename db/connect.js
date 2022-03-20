1ec9d40019e357a24abd998bafa4e14ee65a143e
pate0845 <pate0845@algonquinlive.com>
2022-03-20 19:44:49 -0400

    .

--- db/connect.js
+++ db/connect.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose")
-require('dotenv').config();
+require('dotenv').config({path:"./.env"});
 
 const connectDB=()=>{
     return mongoose.connect(process.env.MONGO_URI,{useNewUrlParser:true,
