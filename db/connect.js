46763f4a0671e9a21c3c9898a8ad2e7f68dcc220
pate0845 <pate0845@algonquinlive.com>
2022-03-21 00:19:36 -0400

    .

--- db/connect.js
+++ db/connect.js
@@ -1,8 +1,9 @@
 const mongoose = require("mongoose")
+
 require('dotenv').config({path:"./.env"});
 
 const connectDB=()=>{
-    return mongoose.connect('mongodb://local:Omnamah3062001@cluster0-shard-00-00.ssloo.mongodb.net:27017,cluster0-shard-00-01.ssloo.mongodb.net:27017,cluster0-shard-00-02.ssloo.mongodb.net:27017/covid19-screening?ssl=true&replicaSet=atlas-gsj7rl-shard-0&authSource=admin&retryWrites=true&w=majority',
+    return mongoose.connect('mongodb+srv://local:Omnamah3062001@cluster0.ssloo.mongodb.net/covid19-screening?retryWrites=true&w=majority',
         (err)=>{
             if(err)console.log(err);
             else console.log('Connected to DB');
