ee502ca5a976851ee2c0635c3083c35e22ba9558
pate0845 <pate0845@algonquinlive.com>
2022-03-20 20:09:25 -0400

    .

--- db/connect.js
+++ db/connect.js
@@ -2,7 +2,9 @@ const mongoose = require("mongoose")
 require('dotenv').config({path:"./.env"});
 
 const connectDB=()=>{
-    return mongoose.connect(process.env.MONGO_URI,
+    return mongoose.connect(process.env.MONGO_URI,{useNewUrlParser:true,
+        useUnifiedTopology:true,
+        useCreateIndex:true},
         (err)=>{
             if(err)console.log(err);
         })
